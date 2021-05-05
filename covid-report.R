2c3426c2307c7d10ed74c955b6fbed09c3d36207
Huseyin Can Minareci <h.minareci@student.uw.edu.pl>
2021-05-05 23:27:07 +0200

    Project started
    First R file created.

---
+++ covid-report.R
@@ -0,0 +1,11 @@
+#############################################
+#          Reproducible Research            #
+#              Final Project                #
+#                                           #
+#        A look at the pandemic             #
+#        for a specified period.            #
+#                                           #
+#   Huseyin Can Minareci                    #
+#   Didem Paloglu                           #
+#                                           #
+#############################################
